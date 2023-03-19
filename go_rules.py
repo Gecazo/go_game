6d2a34f7caf469314526ea09e42f131f22b75a28
Gecazo <gecazo@pop-os.localdomain>
2023-03-19 12:04:52 +0100

    Add capture_stones function and create_board for go_board

--- go_rules.py
+++ go_rules.py
@@ -20,3 +20,20 @@ def is_surrounded(board, row, col):
     if col < board_size-1 and board[row][col+1] == ' ':
         return False
     return True
+
+def capture_stones(board, row, col, player):
+    captured = []
+    # check for captures in all four directions
+    for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+        r, c = row + dr, col + dc
+        if r < 0 or r >= board_size or c < 0 or c >= board_size:
+            continue
+        if board[r][c] == ' ':
+            continue
+        if board[r][c] == player:
+            continue
+        if is_surrounded(board, r, c):
+            captured.append((r, c))
+            # remove captured stones from the board
+            board[r][c] = ' '
+    return captured
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from go_board import board, display_board
-from go_rules import is_valid_move, is_surrounded
+from go_board import board_size, create_board, display_board
+from go_rules import is_valid_move, is_surrounded, capture_stones
 
+board = create_board(board_size)
 players = ['X', 'O']
 current_player = 0
 
@@ -10,8 +11,11 @@ while True:
     move = input("Player " + player + " move (e.g. 'a1'): ")
     col = ord(move[0]) - ord('a')
     row = int(move[1:]) - 1
-    if is_valid_move(board, row, col, player):
+    if is_valid_move(board, row, col, player) and not is_surrounded(board, row, col):
         board[row][col] = player
+        captured = capture_stones(board, row, col, player)
+        if len(captured) > 0:
+            print("Player " + player + " captured " + str(len(captured)) + " stones.")
         current_player = (current_player + 1) % 2
     else:
         print("Invalid move.")
