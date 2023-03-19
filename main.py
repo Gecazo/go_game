1369e5dbba24bdc5b7b67eda103029f35f6d54f4
Gecazo <gecazo@pop-os.localdomain>
2023-03-19 12:20:33 +0100

    Add the game ends when both players pass their turn consecutively

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from go_rules import is_valid_move, is_surrounded, capture_stones
 board = create_board(board_size)
 players = ['X', 'O']
 current_player = 0
+consecutive_passes = 0
 
-while True:
+while consecutive_passes < 2:
     display_board(board)
     player = players[current_player]
     move = input("Player " + player + " move (e.g. 'a1'): ")
+    if move.lower() == "pass":
+        consecutive_passes += 1
+        current_player = (current_player + 1) % 2
+        continue
     col = ord(move[0]) - ord('a')
     row = int(move[1:]) - 1
     if is_valid_move(board, row, col, player) and not is_surrounded(board, row, col):
@@ -17,5 +22,9 @@ while True:
         if len(captured) > 0:
             print("Player " + player + " captured " + str(len(captured)) + " stones.")
         current_player = (current_player + 1) % 2
+        consecutive_passes = 0
     else:
         print("Invalid move.")
+    
+print("Both players passed consecutively. Game over.")
+# TODO: Calculate and display final score
