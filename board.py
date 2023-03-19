e82a78b630a42cf2d013a9d55789eeea99912540
Gecazo <gecazo@pop-os.localdomain>
2023-03-19 11:31:00 +0100

    Add function to display the board in the console

--- board.py
+++ board.py
@@ -1,2 +1,14 @@
 board_size = 9
 board = [[' ' for _ in range(board_size)] for _ in range(board_size)]
+
+
+def display_board(board):
+    print(' ', end=' ')
+    for i in range(board_size):
+        print(chr(ord('a')+i), end=' ')
+    print()
+    for i in range(board_size):
+        print(i+1, end=' ')
+        for j in range(board_size):
+            print(board[i][j], end=' ')
+        print()
